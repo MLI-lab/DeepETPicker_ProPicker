2a39b8d1108822a09dcde11e5d3033d0a641f0fc
SimWdm <wiedemann.simon@yahoo.de>
2025-03-04 10:31:02 +0000

    add possibility to skip coordinates, labels and scp in preprocess

--- bin/preprocess.py
+++ bin/preprocess.py
@@ -19,14 +19,25 @@ if __name__ == "__main__":
 
     with open(args.pre_configs, 'r') as f:
         pre_config = json.loads(''.join(f.readlines()).lstrip('pre_config='))
+     
+    if "skip_coords" not in pre_config.keys():
+        pre_config["skip_coords"] = False
+
+    if "skip_labels" not in pre_config.keys():
+        pre_config["skip_labels"] = False
+
+    if "skip_ocp" not in pre_config.keys():
+        pre_config["skip_ocp"] = False
+
 
     # initial coords
-    coord_gen.coords_gen_show(args=(pre_config["coord_path"],
-                                    pre_config["coord_format"],
-                                    pre_config["base_path"],
-                                    None,
+    if not pre_config["skip_coords"]: 
+      coord_gen.coords_gen_show(args=(pre_config["coord_path"],
+                                          pre_config["coord_format"],
+                                          pre_config["base_path"],
+                                          None,
+                                          )
                                     )
-                              )
 
     # normalization
     norm.norm_show(args=(pre_config["tomo_path"],
@@ -38,27 +49,29 @@ if __name__ == "__main__":
                    )
 
     # generate labels based on coords
-    coords2labels.label_gen_show(args=(pre_config["base_path"],
-                                       pre_config["coord_path"],
-                                       pre_config["coord_format"],
-                                       pre_config["tomo_path"],
-                                       pre_config["tomo_format"],
-                                       pre_config["num_cls"],
-                                       pre_config["label_type"],
-                                       pre_config["label_diameter"],
-                                       None,
-                                       )
-                                 )
+    if not pre_config["skip_labels"]:
+      coords2labels.label_gen_show(args=(pre_config["base_path"],
+                                          pre_config["coord_path"],
+                                          pre_config["coord_format"],
+                                          pre_config["tomo_path"],
+                                          pre_config["tomo_format"],
+                                          pre_config["num_cls"],
+                                          pre_config["label_type"],
+                                          pre_config["label_diameter"],
+                                          None,
+                                          )
+                                    )
 
     # generate occupancy abased on coords
-    coords2labels.label_gen_show(args=(pre_config["base_path"],
-                                       pre_config["coord_path"],
-                                       pre_config["coord_format"],
-                                       pre_config["tomo_path"],
-                                       pre_config["tomo_format"],
-                                       pre_config["num_cls"],
-                                       'data_ocp',
-                                       pre_config["ocp_diameter"],
-                                       None,
-                                       )
-                                 )
+    if not pre_config["skip_ocp"]:
+      coords2labels.label_gen_show(args=(pre_config["base_path"],
+                                          pre_config["coord_path"],
+                                          pre_config["coord_format"],
+                                          pre_config["tomo_path"],
+                                          pre_config["tomo_format"],
+                                          pre_config["num_cls"],
+                                          'data_ocp',
+                                          pre_config["ocp_diameter"],
+                                          None,
+                                          )
+                                    )
